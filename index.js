4deb7163562739543cedb1e2ebdfb0087bb87317
avantikanautiyal <128068458+avantikanautiyal@users.noreply.github.com>
2025-04-23 12:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 5004;
+const PORT = process.env.PORT || 5004;
 const database = require("./connection/mongodb");
 const cookieParser = require("cookie-parser");
 
